0c6d6acce8b0a29e0db617fdb9474b43019763c1
vasilenka <bhmantan@gmail.com>
2018-11-09 11:18:08 +0700

    Updates

---
+++ precache-manifest.b1fbfb74d81d9401ebbaf77fb5499e89.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "12bf326c0f4fea5ff2a6b06f88f08ace",
+    "url": "/static/media/Sentinel-Book.12bf326c.otf"
+  },
+  {
+    "revision": "cce03a63aa0b75670f76",
+    "url": "/static/css/main.5d0fe516.chunk.css"
+  },
+  {
+    "revision": "9088fe3469516c9cbee5",
+    "url": "/static/js/1.9088fe34.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ce5d30387ca7ea6ad55ac1dc6c9a965",
+    "url": "/static/media/Geomanist-Thin.4ce5d303.otf"
+  },
+  {
+    "revision": "9aef73f78bc55e304ab5dd510dd8b40c",
+    "url": "/static/media/Geomanist-ExtraLight.9aef73f7.otf"
+  },
+  {
+    "revision": "7aa547699c1e2d98b20d85116cb9e57e",
+    "url": "/static/media/Geomanist-Light.7aa54769.otf"
+  },
+  {
+    "revision": "de66f0a113d94f90d5b3958bb5744837",
+    "url": "/static/media/Geomanist-Regular.de66f0a1.otf"
+  },
+  {
+    "revision": "c4ef7adafb423b6dc8b4009be00bc105",
+    "url": "/static/media/Geomanist-Book.c4ef7ada.otf"
+  },
+  {
+    "revision": "996e792755ef32ddb65da996d2bbdb7a",
+    "url": "/static/media/Geomanist-Medium.996e7927.otf"
+  },
+  {
+    "revision": "748e4c83bd1adccc610a6d00af37fa0b",
+    "url": "/static/media/Geomanist-Bold.748e4c83.otf"
+  },
+  {
+    "revision": "0c056df65bb1719974106a9c5a290b66",
+    "url": "/static/media/Geomanist-Black.0c056df6.otf"
+  },
+  {
+    "revision": "4125e36d2ba4977379c563d9bcc9e233",
+    "url": "/static/media/Geomanist-Ultra.4125e36d.otf"
+  },
+  {
+    "revision": "a1d65c5b1d1547a34e3e54f964d8f4cd",
+    "url": "/static/media/Sentinel-Medium.a1d65c5b.otf"
+  },
+  {
+    "revision": "40c69937bc2b5f11d3ed6f64b3692bf6",
+    "url": "/static/media/Sentinel-Light.40c69937.otf"
+  },
+  {
+    "revision": "1ba5c38105e4f4c90d168cbef13114ab",
+    "url": "/static/media/Sentinel-BlackItalic.1ba5c381.otf"
+  },
+  {
+    "revision": "cce03a63aa0b75670f76",
+    "url": "/static/js/main.cce03a63.chunk.js"
+  },
+  {
+    "revision": "2ccdefbdae1a3c49c0cfb3e848040b6a",
+    "url": "/static/media/Sentinel-Black.2ccdefbd.otf"
+  },
+  {
+    "revision": "49e282b7916a39fafac04ac4d4c397d2",
+    "url": "/static/media/Sentinel-MediumItalic.49e282b7.otf"
+  },
+  {
+    "revision": "a83d81a7774d0a128047ef2294d66703",
+    "url": "/static/media/Sentinel-Semibold.a83d81a7.otf"
+  },
+  {
+    "revision": "52d2cb71b40929e5263bcd113536133a",
+    "url": "/static/media/Sentinel-Bold.52d2cb71.otf"
+  },
+  {
+    "revision": "e999b6851f1cef1dae8690b5d87fc8f0",
+    "url": "/static/media/Sentinel-LightItalic.e999b685.otf"
+  },
+  {
+    "revision": "46e4ca8897ba13d1467632ef1a475314",
+    "url": "/static/media/Sentinel-SemiboldItalic.46e4ca88.otf"
+  },
+  {
+    "revision": "251a7d2bf4b30fd63941c04d20150984",
+    "url": "/static/media/Sentinel-BoldItalic.251a7d2b.otf"
+  },
+  {
+    "revision": "e554c4737726b7cc5ad5d1a6a3678515",
+    "url": "/static/media/Sentinel-BookItalic.e554c473.otf"
+  },
+  {
+    "revision": "4395b29d9210bd75051ebce1e66e186d",
+    "url": "/static/media/AirbnbCereal-Black.4395b29d.ttf"
+  },
+  {
+    "revision": "0a0cbe1eca15f71b7187b38b1173eb98",
+    "url": "/static/media/AirbnbCereal-ExtraBold.0a0cbe1e.ttf"
+  },
+  {
+    "revision": "c94c29feb80b02db176028d16fc634d2",
+    "url": "/static/media/AirbnbCereal-Bold.c94c29fe.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/static/media/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "86b15ac031e0507ad790afafd9adf7f7",
+    "url": "/static/media/AirbnbCereal-Book.86b15ac0.ttf"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "/static/media/AirbnbCereal-Light.1f9c7d32.ttf"
+  },
+  {
+    "revision": "ed9d199da954c7f4f6a407b85d80bb3c",
+    "url": "/index.html"
+  }
+];
