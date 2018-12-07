d5d927fab108f04162e2b14f228e29e45b24e770
vasilenka <bhmantan@gmail.com>
2018-12-07 14:33:15 +0700

    add logo and remove unused stuff

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import styles from './App.module.scss';
 import classnames from 'classnames';
+import tinker from './tink.png';
 
 import Text from './components/Text/Text';
-import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className={classnames(styles.root)}>
-        <Navbar />
-        <Text className={classnames(styles.greet)} display2 component="h1">
+        <img className={styles.logo} src={tinker} alt="" />
+        <Text heading1 component="h1">
           tinker.
         </Text>
       </div>
